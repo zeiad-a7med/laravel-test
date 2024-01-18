5f55244927112101c3c68db67de46833587735ac
zeiad-a7med <zeiadahmed194@gmail.com>
2024-01-18 02:27:26 +0200

    creating product and category models to use it in CRUD operations with DB

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,6 @@ use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 
 ROute::group(['prefix' => LaravelLocalization::setLocale(),'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]],function(){
-    Route::get('/', function () {
-        return view('welcome');
-    });    
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Auth::routes();
 });
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
